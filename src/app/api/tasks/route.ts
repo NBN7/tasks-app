78cc98fd058effc72e95c20710f3aa20f73144fb
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-20 04:18:32 -0300

    add try catch to /api/tasks

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -3,20 +3,28 @@ import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
 export async function GET() {
-  const tasks = await prisma.task.findMany();
+  try {
+    const tasks = await prisma.task.findMany();
 
-  return NextResponse.json(tasks);
+    return NextResponse.json(tasks);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
 }
 
 export async function POST(req: NextRequest) {
-  const { title, description } = await req.json();
+  try {
+    const { title, description } = await req.json();
 
-  const createdTask = await prisma.task.create({
-    data: {
-      title,
-      description,
-    },
-  });
+    const createdTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+      },
+    });
 
-  return NextResponse.json(createdTask);
+    return NextResponse.json(createdTask);
+  } catch (error) {
+    return NextResponse.json(error);
+  }
 }
