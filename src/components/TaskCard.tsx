b8575944d0ecb0063c2014229cd247eb654e5693
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-21 01:16:23 -0300

    update task card ui

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -9,13 +9,15 @@ import { DeleteTaskButton } from "./DeleteTaskButton";
 
 export const TaskCard = ({ task }: TaskCardProps) => {
   return (
-    <div
-      className={`w-full flex items-center justify-between gradient p-4 rounded-md`}
-    >
+    <div className="w-full flex items-center justify-between bg-[#212121] border border-[#303030] p-4 rounded-md">
       <section className="pointer-events-none flex flex-col gap-2 overflow-hidden">
-        <h2 className="text-lg font-semibold truncate">{task.title}</h2>
+        <h2 className="text-[#0070f0] text-xl font-semibold truncate">
+          {task.title}
+        </h2>
+
         <p className="truncate">{task.description}</p>
-        <p className="text-xs">
+
+        <p className="text-[#707070] text-xs">
           {new Date(task.createdAt).toLocaleDateString()}
         </p>
       </section>
