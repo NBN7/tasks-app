6ddcf5973e5b536e1fee0a8087cc3a12027f2293
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-21 03:19:16 -0300

    update function type

--- src/utils/getAllTasks.ts
+++ src/utils/getAllTasks.ts
@@ -1,8 +1,10 @@
+import type { Task } from "@/types/task";
+
 export const getAllTasks = async () => {
   const res = await fetch("http://localhost:3000/api/tasks", {
     cache: "no-cache",
   });
-  const data = await res.json();
+  const data: Task[] = await res.json();
 
   return data;
 };
