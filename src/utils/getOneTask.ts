5769bd26d8b4cbc630785c542fd580c85cb6aa02
NBN7 <105465105+NBN7@users.noreply.github.com>
2023-10-21 04:27:19 -0300

    update function type

--- src/utils/getOneTask.ts
+++ src/utils/getOneTask.ts
@@ -2,7 +2,7 @@ import { Task } from "@/types/task";
 
 export const getOneTask = async (id: Number) => {
   const res = await fetch(`http://localhost:3000/api/tasks/${id}`);
-  const data: Task[] = await res.json();
+  const data: Task = await res.json();
 
   return data;
 };
